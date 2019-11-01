fff1092b84ca8829b4726315e87095494629c654
Hiro_Alpha <hmkalpha0120@gmail.com>
2019-11-02 04:26:07 +0900

    Sixth Commit
    文字の周りの白線完成（アニメ準拠）
    白線内色付け
    Action Bar削除（本当に必要？）

--- app/src/main/java/com/hiro_a/naruko_animation/CanvasView.java
+++ app/src/main/java/com/hiro_a/naruko_animation/CanvasView.java
@@ -18,8 +18,8 @@ import static java.lang.Math.sin;
 public class CanvasView extends View {
     int count = 0;  //文字列受け取り回数
     int textSize = 30;  //文字サイズ
-    int radius = 700;   //回転半径
-    int chatCircleRedius = 25;  //UI白丸半径
+    int radius = 400;   //回転半径
+    int chatCircleRedius = 20;  //UI白丸半径
     float btmArcLeft, btmArcTop, btmArcRight, btmArcBttom;
     float topArcLeft, topArcTop, topArcRight, topArcBttom;
     double rightCircleSin, rightCircleCos;
@@ -28,10 +28,13 @@ public class CanvasView extends View {
 
     Paint textPaint;
     Paint pathPaint;
-    Paint graphicPaint;
+    Paint graphicPain_Line;
+    Paint graphicPaint_Colored, graphicPaint_Colored_FILL;
 
     Path textPath;
     Path graphicPath;
+    Path graphicPath_Line;
+    Path graphicPath_Colored;
 
     public CanvasView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -48,11 +51,22 @@ public class CanvasView extends View {
         pathPaint.setColor(Color.BLACK);
         pathPaint.setStrokeWidth(1);
 
-        //UIPath設定
-        graphicPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        graphicPaint.setStyle(Paint.Style.STROKE);
-        graphicPaint.setColor(Color.WHITE);
-        graphicPaint.setStrokeWidth(5);
+        //UI下色Path設定
+        graphicPaint_Colored_FILL = new Paint(Paint.ANTI_ALIAS_FLAG);
+        graphicPaint_Colored_FILL.setStyle(Paint.Style.FILL);
+        graphicPaint_Colored_FILL.setColor(Color.rgb(255,192,203));
+
+        //UI下色Path設定
+        graphicPaint_Colored = new Paint(Paint.ANTI_ALIAS_FLAG);
+        graphicPaint_Colored.setStyle(Paint.Style.STROKE);
+        graphicPaint_Colored.setColor(Color.rgb(255,192,203));
+        graphicPaint_Colored.setStrokeWidth(chatCircleRedius*2);
+
+        //UI白線Path設定
+        graphicPain_Line = new Paint(Paint.ANTI_ALIAS_FLAG);
+        graphicPain_Line.setStyle(Paint.Style.STROKE);
+        graphicPain_Line.setColor(Color.WHITE);
+        graphicPain_Line.setStrokeWidth(5);
     }
 
     @Override
@@ -61,19 +75,30 @@ public class CanvasView extends View {
 
         //文字列補助線
         textPath = new Path();
-        textPath.addCircle(-400, 0, radius, Path.Direction.CCW);    //円形のパスをx-400、y0を中心として描画、反時計回り
+        textPath.addCircle(-45, 0, radius, Path.Direction.CCW);    //円形のパスをx-45、y0を中心として描画、反時計回り
         canvas.drawPath(textPath, pathPaint);
 
         if (drawMode){
-            //UI白線
+            //UI下色
+            graphicPath_Colored = new Path();
+            RectF coloredArc = new RectF(topArcLeft-chatCircleRedius, topArcTop-chatCircleRedius, topArcRight+chatCircleRedius, topArcBttom+chatCircleRedius);   //円弧範囲
+            graphicPath_Colored.addArc(coloredArc, 270, 90); //円弧
+            canvas.drawPath(graphicPath_Colored, graphicPaint_Colored);
+
+            //共通項
             graphicPath = new Path();
-            graphicPath.addCircle(radius-400-(textSize/2), 0, chatCircleRedius, Path.Direction.CW); //左白丸
-            graphicPath.addCircle((float) rightCircleCos, -((float)rightCircleSin), chatCircleRedius, Path.Direction.CW); //右白丸
+            graphicPath.addCircle(radius-45-(textSize/2), 0, chatCircleRedius, Path.Direction.CW); //左丸
+            graphicPath.addCircle((float) rightCircleCos, -((float)rightCircleSin), chatCircleRedius, Path.Direction.CW); //右丸
+            canvas.drawPath(graphicPath, graphicPaint_Colored_FILL);
+            canvas.drawPath(graphicPath, graphicPain_Line);
+
+            //UI白線
+            graphicPath_Line = new Path();
             RectF topArcRect = new RectF(topArcLeft, topArcTop, topArcRight, topArcBttom);   //円弧上側範囲
-            graphicPath.addArc(topArcRect, 315, 45); //円弧上側
+            graphicPath_Line.addArc(topArcRect, 270, 90); //円弧上側
             RectF bottomArcRect = new RectF(btmArcLeft, btmArcTop, btmArcRight, btmArcBttom);  //円弧下側範囲
-            graphicPath.addArc(bottomArcRect, 315, 45); //円弧下側
-            canvas.drawPath(graphicPath, graphicPaint);
+            graphicPath_Line.addArc(bottomArcRect, 270, 90); //円弧下側
+            canvas.drawPath(graphicPath_Line, graphicPain_Line);
 
             //曲線文字列
             canvas.drawTextOnPath(text, textPath, 40, 0, textPaint);
@@ -88,20 +113,20 @@ public class CanvasView extends View {
         if (count < 6){
             count++;
         }
-        radius = ((count-1) * (textSize+20)) + 700;
+        radius = ((count-1) * (textSize+10)) + 400;
 
         //右白丸描画用座標
-        rightCircleSin = (sin(Math.toRadians(45))*(radius-(textSize/2)));
-        rightCircleCos = (cos(Math.toRadians(45))*(radius-(textSize/2))) - 400;
+        rightCircleSin = (sin(Math.toRadians(90))*(radius-(textSize/2)));
+        rightCircleCos = (cos(Math.toRadians(90))*(radius-(textSize/2))) - 45;
         //円弧描画用座標（上側）
-        topArcLeft = -(400+radius-(textSize/2)-chatCircleRedius);
+        topArcLeft = -(45+radius-(textSize/2)-chatCircleRedius);
         topArcTop = -(radius-(textSize/2)-chatCircleRedius);
-        topArcRight = radius-400-(textSize/2)-chatCircleRedius;
+        topArcRight = radius-45-(textSize/2)-chatCircleRedius;
         topArcBttom = radius-(textSize/2)-chatCircleRedius;
         //円弧描画用座標（下側）
-        btmArcLeft = -(400+radius-(textSize/2)+chatCircleRedius);
+        btmArcLeft = -(45+radius-(textSize/2)+chatCircleRedius);
         btmArcTop = -(radius-(textSize/2)+chatCircleRedius);
-        btmArcRight = radius-400-(textSize/2)+chatCircleRedius;
+        btmArcRight = radius-45-(textSize/2)+chatCircleRedius;
         btmArcBttom = radius-(textSize/2)+chatCircleRedius;
 
         drawMode = true;
--- app/src/main/java/com/hiro_a/naruko_animation/CanvasView2.java
+++ app/src/main/java/com/hiro_a/naruko_animation/CanvasView2.java
@@ -16,8 +16,8 @@ import static java.lang.Math.sin;
 
 public class CanvasView2 extends View {
     int textSize = 30;  //文字サイズ
-    int radius = 700;   //回転半径
-    int chatCircleRedius = 25;  //UI白丸半径
+    int radius = 400;   //回転半径
+    int chatCircleRedius = 20;  //UI白丸半径
     String text = "";
     ArrayList<String> textHolder = new ArrayList<String>(); //過去の文字列格納用Array
     float btmArcLeft, btmArcTop, btmArcRight, btmArcBttom;
@@ -26,10 +26,13 @@ public class CanvasView2 extends View {
 
     Paint textPaint;
     Paint pathPaint;
-    Paint graphicPaint;
+    Paint graphicPain_Line;
+    Paint graphicPaint_Colored, graphicPaint_Colored_FILL;
 
     Path textPath;
     Path graphicPath;
+    Path graphicPath_Line;
+    Path graphicPath_Colored;
 
     public CanvasView2(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -46,11 +49,22 @@ public class CanvasView2 extends View {
         pathPaint.setColor(Color.RED);
         pathPaint.setStrokeWidth(1);
 
-        //UIPath設定
-        graphicPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        graphicPaint.setStyle(Paint.Style.STROKE);
-        graphicPaint.setColor(Color.WHITE);
-        graphicPaint.setStrokeWidth(5);
+        //UI下色Path設定
+        graphicPaint_Colored_FILL = new Paint(Paint.ANTI_ALIAS_FLAG);
+        graphicPaint_Colored_FILL.setStyle(Paint.Style.FILL);
+        graphicPaint_Colored_FILL.setColor(Color.rgb(255,192,203));
+
+        //UI下色Path設定
+        graphicPaint_Colored = new Paint(Paint.ANTI_ALIAS_FLAG);
+        graphicPaint_Colored.setStyle(Paint.Style.STROKE);
+        graphicPaint_Colored.setColor(Color.rgb(255,192,203));
+        graphicPaint_Colored.setStrokeWidth(chatCircleRedius*2);
+
+        //UI白線Path設定
+        graphicPain_Line = new Paint(Paint.ANTI_ALIAS_FLAG);
+        graphicPain_Line.setStyle(Paint.Style.STROKE);
+        graphicPain_Line.setColor(Color.WHITE);
+        graphicPain_Line.setStrokeWidth(5);
     }
 
     @Override
@@ -66,36 +80,47 @@ public class CanvasView2 extends View {
 
             //入力されたものより1つ前の文字列から最も古いものまで
             for (int i=textSpan; i<textHolder.size()-1; i++){
-                radius = (multiplier * (textSize+20)) + 700;    //TextSize分半径をずらす
+                radius = (multiplier * (textSize+10)) + 400;    //TextSize分半径をずらす
 
                 //文字列補助線
                 textPath = new Path();
-                textPath.addCircle(-400, 0, radius, Path.Direction.CCW);    //円形のパスをx-400、y0を中心として描画、反時計回り
+                textPath.addCircle(-45, 0, radius, Path.Direction.CCW);    //円形のパスをx-400、y0を中心として描画、反時計回り
                 canvas.drawPath(textPath, pathPaint);
 
                 //右白丸描画用座標
-                rightCircleSin = (sin(Math.toRadians(45))*(radius-(textSize/2)));
-                rightCircleCos = (cos(Math.toRadians(45))*(radius-(textSize/2))) - 400;
+                rightCircleSin = (sin(Math.toRadians(90))*(radius-(textSize/2)));
+                rightCircleCos = (cos(Math.toRadians(90))*(radius-(textSize/2))) - 45;
                 //円弧描画用座標（上側）
-                topArcLeft = -(400+radius-(textSize/2)-chatCircleRedius);
+                topArcLeft = -(45+radius-(textSize/2)-chatCircleRedius);
                 topArcTop = -(radius-(textSize/2)-chatCircleRedius);
-                topArcRight = radius-400-(textSize/2)-chatCircleRedius;
+                topArcRight = radius-45-(textSize/2)-chatCircleRedius;
                 topArcBttom = radius-(textSize/2)-chatCircleRedius;
                 //円弧描画用座標（下側）
-                btmArcLeft = -(400+radius-(textSize/2)+chatCircleRedius);
+                btmArcLeft = -(45+radius-(textSize/2)+chatCircleRedius);
                 btmArcTop = -(radius-(textSize/2)+chatCircleRedius);
-                btmArcRight = radius-400-(textSize/2)+chatCircleRedius;
+                btmArcRight = radius-45-(textSize/2)+chatCircleRedius;
                 btmArcBttom = radius-(textSize/2)+chatCircleRedius;
 
-                //UI白線
+                //UI下色
+                graphicPath_Colored = new Path();
+                RectF coloredArc = new RectF(topArcLeft-chatCircleRedius, topArcTop-chatCircleRedius, topArcRight+chatCircleRedius, topArcBttom+chatCircleRedius);   //円弧範囲
+                graphicPath_Colored.addArc(coloredArc, 270, 90); //円弧
+                canvas.drawPath(graphicPath_Colored, graphicPaint_Colored);
+
+                //共通項
                 graphicPath = new Path();
-                graphicPath.addCircle(radius-400-(textSize/2), 0, chatCircleRedius, Path.Direction.CW); //左白丸
-                graphicPath.addCircle((float) rightCircleCos, -((float)rightCircleSin), chatCircleRedius, Path.Direction.CW); //右白丸
+                graphicPath.addCircle(radius-45-(textSize/2), 0, chatCircleRedius, Path.Direction.CW); //左丸
+                graphicPath.addCircle((float) rightCircleCos, -((float)rightCircleSin), chatCircleRedius, Path.Direction.CW); //右丸
+                canvas.drawPath(graphicPath, graphicPaint_Colored_FILL);
+                canvas.drawPath(graphicPath, graphicPain_Line);
+
+                //UI白線
+                graphicPath_Line = new Path();
                 RectF topArcRect = new RectF(topArcLeft, topArcTop, topArcRight, topArcBttom);   //円弧上側範囲
-                graphicPath.addArc(topArcRect, 315, 45); //円弧上側
+                graphicPath_Line.addArc(topArcRect, 270, 90); //円弧上側
                 RectF bottomArcRect = new RectF(btmArcLeft, btmArcTop, btmArcRight, btmArcBttom);  //円弧下側範囲
-                graphicPath.addArc(bottomArcRect, 315, 45); //円弧下側
-                canvas.drawPath(graphicPath, graphicPaint);
+                graphicPath_Line.addArc(bottomArcRect, 270, 90); //円弧下側
+                canvas.drawPath(graphicPath_Line, graphicPain_Line);
 
                 //曲線文字列
                 canvas.drawTextOnPath(textHolder.get(i), textPath, 40, 0, textPaint);
